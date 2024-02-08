371cd111162f16a8e70899e1cd0b2f767e7d88a9
cryptony8055 <adityanarayan8055@gmail.com>
2024-02-08 17:17:16 +0530

    php ver of registration form

--- day3/task1.php
+++ day3/task1.php
@@ -1,3 +1,15 @@
+<?php
+
+if(!empty($_POST)){
+    if($_POST['submit']==1){
+        var_dump($_POST);
+    }
+    
+}
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,93 +19,92 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
     <title>Form Design</title>
   </head>
   <body>
-   <div class="container-fluid bg-dark text-light py-3">
+   <div class="container-fluid text-dark py-3">
        <header class="text-center">
            <h1 class="display-6">Registration page</h1>
        </header>
    </div>
-   <section class="container my-2 bg-dark w-50 text-light rounded-3 p-2">
-    <form class="row g-3 p-3">
+   <section class="container my-2 w-50 text-dark rounded-3 p-2">
+    <form action="#" method="post" class="row g-3 p-3">
         <div class="col-md-4">
-            <label for="validationDefault01" class="form-label">First name</label>
-            <input type="text" class="form-control" id="validationDefault01" placeholder="Enter Fname..." required>
+            <label for="fname" class="form-label">First name</label>
+            <input type="text" class="form-control" name="fname" placeholder="Enter Fname..." required>
           </div>
           <div class="col-md-4">
-            <label for="validationDefault02" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="validationDefault02" placeholder="Enter Lname..." required>
+            <label for="lname" class="form-label">Last name</label>
+            <input type="text" class="form-control" name="lname" placeholder="Enter Lname..." required>
           </div>
           <div class="col-md-4">
-            <label for="validationDefault03" class="form-label">Age</label>
-            <input type="number" class="form-control" id="validationDefault03" placeholder="Enter Age..." required>
+            <label for="age" class="form-label">Age</label>
+            <input type="number" class="form-control" name="age" placeholder="Enter Age..." required>
           </div>
           <div class="col-md-4">
             <label for="inputGender" class="form-label">Gender</label>
-            <select id="inputGender" class="form-select">
-              <option selected>Choose...</option>
-              <option>male</option>
-              <option>female</option>
-              <option>others</option>
+            <select name="inputGender" class="form-select">
+              <option selected value=''>Choose...</option>
+              <option value="male">male</option>
+              <option value="female">female</option>
+              <option value="other">other</option>
             </select>
           </div>
           <div class="col-md-4">
             <label for="Profession" class="form-label">Profession</label>
-            <select id="Profession" class="form-select">
-              <option selected>Choose...</option>
-              <option>Salaried</option>
-              <option>Unemployed</option>
-              <option>Student</option>
-              <option>Business</option>
+            <select name="Profession" class="form-select">
+              <option selected value=''>Choose...</option>
+              <option value="salaried">Salaried</option>
+              <option value="unemployed">Unemployed</option>
+              <option value="student">Student</option>
+              <option value="business">Business</option>
             </select>
           </div>
           <div class="col-md-4">
             <label for="qualification" class="form-label">Highest qualification</label>
-            <select id="qualification" class="form-select">
-              <option selected>Choose...</option>
-              <option>Graduation</option>
-              <option>Matriculation</option>
-              <option>Post matriculation</option>
+            <select name="qualification" class="form-select">
+              <option selected value=''>Choose...</option>
+              <option value="graduation">Graduation</option>
+              <option value="marticulation">Matriculation</option>
+              <option value="postmarticulation">Post matriculation</option>
             </select>
           </div>
           <div class="col-md-6">
             <label for="marritalStatus" class="form-label">Marrital Status</label>
-            <select id="marritalStatus" class="form-select">
-              <option selected>-Na-</option>
-              <option>Married</option>
-              <option>Unmarried</option>
-              <option></option>
+            <select name="marritalStatus" class="form-select">
+              <option selected value=''>-Na-</option>
+              <option value="married">Married</option>
+              <option value="unmarried">Unmarried</option>
             </select>
           </div>  
         <div class="col-md-6">
             <label for="uploadPhoto" class="form-label">upload your photo</label>
-            <input type="file" class="form-control">
+            <input type="file" class="form-control" name="photo">
           </div>
         <div class="col-md-6">
-          <label for="inputEmail4" class="form-label">Email</label>
-          <input type="email" class="form-control" id="inputEmail4">
+          <label for="inputEmail" class="form-label">Email</label>
+          <input type="email" class="form-control" name="inputEmail" required>
         </div>
         <div class="col-md-6">
           <label for="inputPhone" class="form-label">phone</label>
-          <input type="tel" class="form-control" id="inputPhone">
+          <input type="tel" class="form-control" name="inputPhone" required>
         </div>
         <div class="col-12">
           <label for="inputAddress" class="form-label">Address</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+          <input type="text" class="form-control" name="inputAddress" placeholder="1234 Main St" required>
         </div>
         <div class="col-12">
           <label for="inputAddress2" class="form-label">Address 2</label>
-          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+          <input type="text" class="form-control" name="inputAddress2" placeholder="Apartment, studio, or floor">
         </div>
         <div class="col-md-6">
           <label for="inputCity" class="form-label">City</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" class="form-control" name="inputCity">
         </div>
         <div class="col-md-4">
           <label for="inputState" class="form-label">State</label>
-          <select id="inputState" class="form-select">
+          <select name="inputState" class="form-select">
             <option selected>Choose...</option>
             <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
             <option value="Andhra Pradesh">Andhra Pradesh</option>
@@ -135,18 +146,18 @@
         </div>
         <div class="col-md-2">
           <label for="inputZip" class="form-label">Zip</label>
-          <input type="number" class="form-control" id="inputZip">
+          <input type="number" class="form-control" name="inputZip">
         </div>
         <div class="col-12">
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <input class="form-check-input" type="checkbox" name="gridCheck" value="1">
             <label class="form-check-label" for="gridCheck">
               Check me out
             </label>
           </div>
         </div>
         <div class="col-12">
-          <button type="submit" class="btn btn-primary">Sign in</button>
+          <button type="submit" class="btn btn-primary" value="1" name="submit">submit</button>
         </div>
       </form>
    </section>
